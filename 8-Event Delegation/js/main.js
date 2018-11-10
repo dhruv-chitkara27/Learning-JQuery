1fb8c06c30175e8a967cdf034e8467a075dc533d
dhruv-chitkara <dhruvchitkara27@gmail.com>
2018-11-10 18:11:05 +0530

    Added events

---
+++ 8-Event Delegation/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('.box').on('click','.box-button',function(){
+		$(this).parent().toggleClass('highlight');
+	});
+});
---
+++ 9-Change Event/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#select-menu').on('change',function(){
+		let name = $('#select-menu option:selected').text();
+		let distance = $('#select-menu option:selected').val();
+		let price = $('#select-menu option:selected').data('price');
+
+		if(distance) {
+		$('#feedback-message').text('You are signing up for a ' + name + ', which costs ' + price + ', to a distance of ' + distance + 'km');
+		}
+		else {
+			$("#feedback-message").empty();
+		}
+	});
+});
